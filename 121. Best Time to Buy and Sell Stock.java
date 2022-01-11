35ad59fcda5d5eb1ae5bd4ff23a039c49d4a05bf
Aaditya Sudhir Joshi <64467870+Aaditya55@users.noreply.github.com>
2022-01-11 23:41:29 +0530

    Create 121. Best Time to Buy and Sell Stock.java

---
+++ 121. Best Time to Buy and Sell Stock.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int sub = prices[0];
+        for(int cp : prices){  
+            sub = Math.min(sub,cp);
+            max = Math.max(max,cp-sub);
+        }
+        
+        return max;
+    }
+}
+
+/*
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+*/
