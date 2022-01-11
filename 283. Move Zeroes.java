3b130b5bd6ff32c04cc4aab7d556ecf04e454a68
Aaditya Sudhir Joshi <64467870+Aaditya55@users.noreply.github.com>
2022-01-12 00:02:04 +0530

    Create 283. Move Zeroes.java

---
+++ 283. Move Zeroes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        if(n==0 || n==0) return ;
+        int left = 0;
+        int right = 0;
+        int temp;
+       while(right < n){
+           if(nums[right] == 0){
+               ++right;
+           }else{
+               temp = nums[left];
+               nums[left] = nums[right];
+               nums[right] = temp ;
+               ++left;
+               ++right;
+           }
+       }
+        
+    }
+}
